8a417343715084879fa68c5d203914463659aa89
Naitik Shah <n@daaku.org>
2020-08-18 10:39:21 +0400

    switch to lein

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject daaku/schema "0.1.2"
+  :description "Schema validation and transformation, using simple functions."
+  :url "https://github.com/daaku/schema"
+  :scm {:name "git" :url "https://github.com/daaku/schema"}
+  :license {:name "MIT License"}
+  :dependencies [[org.clojure/clojure "1.10.1"]])
