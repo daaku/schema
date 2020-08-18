40bdbb73ad4ad237bec0c47b4c7b46611d415c45
Naitik Shah <n@daaku.org>
2020-08-18 10:39:52 +0400

    bump to 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject daaku/schema "0.1.2"
+(defproject daaku/schema "0.1.3"
   :description "Schema validation and transformation, using simple functions."
   :url "https://github.com/daaku/schema"
   :scm {:name "git" :url "https://github.com/daaku/schema"}
