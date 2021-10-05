7622aab1b4658ba9518cff292a6881be13e870dd
Naitik Shah <n@daaku.org>
2021-10-05 20:30:12 +0400

    upgrade deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/daaku/schema"
   :scm {:name "git" :url "https://github.com/daaku/schema"}
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
   :plugins [[lein-doo "0.1.11"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.773"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.879"]]}}
   :cljsbuild {:builds {:test {:source-paths ["src" "test"]
                               :compiler {:target :nodejs
                                          :output-to "target/schema.js"
